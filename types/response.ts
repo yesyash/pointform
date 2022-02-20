b6ba233cb331eb698f9c844774d5b85d7f07afca
ruthvik <ruthvik.web@gmail.com>
2022-02-20 10:33:58 +0530

    I-0: Update response type
    Added responses property which is an array of ResponseItem, omitted _id

--- types/response.ts
+++ types/response.ts
@@ -1,7 +1,17 @@
-export interface Response {
+export interface ResponseItem {
     _id: string
     questionId: string
-    answerId: string
+    answer: string
+}
+
+export type CreateResponseItemType = Omit<ResponseItem, '_id'>
+
+export interface Response {
+    _id: string
+    responseId: string
+    responses: CreateResponseItemType[]
     startedOn: Date
     finishedAt: Date
 }
+
+export type CreateResponseType = Omit<Response, '_id'>
